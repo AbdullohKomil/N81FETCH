90fe4cbe142a63d19e2b4877a6b5976b3080d3e9
AbdullohKomil <akomiljonov@icloud.com>
2023-01-05 15:41:50 +0500

    add next and prev btn

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 let elInput = document.querySelector(".js-input");
 let elForm = document.querySelector("form");
 
+let elPrevBtn = document.querySelector(".js-prev-btn");
+let elNextBtn = document.querySelector(".js-next-btn");
+
 // let newArr = [];
+
+let activePage = 36;
 
 const render = (array, node) => {
   node.innerHTML = "";
@@ -22,20 +27,52 @@ const render = (array, node) => {
     newItem.appendChild(newTitle);
     newItem.appendChild(newYear);
     newItem.appendChild(newType);
-    newItem.setAttribute('class', 'col-12 col-xl-5 text-center mt-3 bg-dark text-light')
+    newItem.setAttribute(
+      "class",
+      "col-12 col-xl-5 text-center mt-3 bg-dark text-light"
+    );
     elList.appendChild(newItem);
   }
 };
 
-elForm.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-  fetch(`https://www.omdbapi.com/?apikey=ba5d0120&s=${elInput.value}`)
+const fetchFunc = () => {
+  if (activePage == 1) {
+    elPrevBtn.setAttribute("disabled", "true");
+  } else {
+    elPrevBtn.removeAttribute("disabled");
+  }
+
+  fetch(
+    `https://www.omdbapi.com/?apikey=ba5d0120&s=${elInput.value}&page=${activePage}`
+  )
     .then((response) => response.json())
     .then((data) => {
+      console.log(data);
       if (data) {
         render(data.Search, elList);
+        console.log(data);
+      }
+
+      if (activePage == Math.ceil(data.totalResults / 10)) {
+        elNextBtn.setAttribute("disabled", "true");
+      } else {
+        elNextBtn.removeAttribute("disabled");
       }
     });
+};
 
-  elInput.value = "";
+elForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  fetchFunc();
+  // elInput.value = "";
+});
+
+elPrevBtn.addEventListener("click", () => {
+  activePage--;
+  fetchFunc();
+});
+
+elNextBtn.addEventListener("click", () => {
+  activePage++;
+  fetchFunc();
 });
